1ae62fb67c85b4ba534b1713ddbf0f9e6560929f
Barbarpotato <darmawanjr88@gmail.com>
2024-12-14 03:40:08 +0000

    Deploy static content to GitHub Pages

---
+++ OkeZZHF4i-HMnk-WUJ5F-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-6b5fb578ab874fe8.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],"/labs/[slug]":["static/chunks/pages/labs/[slug]-b6b888b0a2f30cff.js"],sortedPages:["/","/_app","/_error","/labs/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
