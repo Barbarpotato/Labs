9c2d86211cc76b4bb9e5fcfb6e60e9d8b519fefb
Barbarpotato <Barbarpotato@users.noreply.github.com>
2024-12-14 07:36:34 +0000

    deploy: 36c5b986e8745c3b2393b0b5e15123d5093e5e63

---
+++ _next/static/X6fUeT2Rn2-EUlKnwqnlU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-2de8416e563288a2.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],"/[slug]":["static/chunks/pages/[slug]-aa5054141f55cf01.js"],sortedPages:["/","/_app","/_error","/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
