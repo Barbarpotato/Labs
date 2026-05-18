8824ac17efd072aa91fffc6c459ccff3c88a0c15
Barbarpotato <darmawanjr88@gmail.com>
2026-05-18 20:39:32 +0800

    remove darwin

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 // Core Modules
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Darwin } from "personal-shared-layout"
 import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
@@ -12,7 +10,6 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
     <QueryClientProvider client={queryClient}>
-      <Darwin />
       <App />
     </QueryClientProvider>
   </ChakraProvider>
